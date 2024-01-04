535beb4363149afa038aa19bfab540bd80b7374b
Anusha Upadhyay <152385509+Anusha75@users.noreply.github.com>
2024-01-04 22:58:52 +0545

    Add files via upload

---
+++ E-Voting system/Routes/Dashboard.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>E-Voting System - Dashboard</title>
+
+    </head>
+    <body>
+        <button>Back</button>
+        <button>Log out</button>
+        <h1>E-Voting System</h1>
+        <hr>
+        <div id="Profile"></div>
+        <div id="Group"></div>
+    </body>
+</html>
